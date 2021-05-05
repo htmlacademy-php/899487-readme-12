e8f5a22d2451a62e03df95dfb19d85e9a3b94ef1
Sergei <rudserg39@gmail.com>
2021-05-05 17:12:14 +0200

    Make corrections

--- index.php
+++ index.php
@@ -240,97 +240,76 @@
             </div>
         </div>
         <div class="popular__posts">
-            <div class="visually-hidden" id="donor">
-                <!--содержимое для поста-цитаты-->
-                <blockquote>
-                    <p>
-                        <!--здесь текст-->
-                    </p>
-                    <cite>Неизвестный Автор</cite>
-                </blockquote>
-
-                <!--содержимое для поста-ссылки-->
-                <div class="post-link__wrapper">
-                    <a class="post-link__external" href="http://" title="Перейти по ссылке">
-                        <div class="post-link__info-wrapper">
-                            <div class="post-link__icon-wrapper">
-                                <img src="https://www.google.com/s2/favicons?domain=vitadental.ru" alt="Иконка">
-                            </div>
-                            <div class="post-link__info">
-                                <h3><!--здесь заголовок--></h3>
-                            </div>
-                        </div>
-                        <span><!--здесь ссылка--></span>
-                    </a>
-                </div>
-
-                <!--содержимое для поста-фото-->
-                <div class="post-photo__image-wrapper">
-                    <img src="img/" alt="Фото от пользователя" width="360" height="240">
-                </div>
-
-                <!--содержимое для поста-видео-->
-                <div class="post-video__block">
-                    <div class="post-video__preview">
-                        <?=embed_youtube_cover(/* вставьте ссылку на видео */); ?>
-                        <img src="img/coast-medium.jpg" alt="Превью к видео" width="360" height="188">
-                    </div>
-                    <a href="post-details.html" class="post-video__play-big button">
-                        <svg class="post-video__play-big-icon" width="14" height="14">
-                            <use xlink:href="#icon-video-play-big"></use>
-                        </svg>
-                        <span class="visually-hidden">Запустить проигрыватель</span>
-                    </a>
-                </div>
-
-                <!--содержимое для поста-текста-->
-                <p><!--здесь текст--></p>
-            </div>
-            <?php foreach ($arr as $item): ?>
-            <article class="popular__post post <?=  item['type'] ?>">
-                <header class="post__header">
-                    <h2><?=  $item['title']?></h2>
-                </header>
+          <?php foreach ($array as $item): ?>
+          <article class="popular__post post <?= item['type'] ?>">
+              <header class="post__header">
+                <h2><?= $item['title'] ?></h2>
+              </header>
                 <div class="post__main">
+                  <?php if ($item['type'] === 'post-quote'): ?>
+                    <blockquote>
+                      <p><?= $item['content'] ?></p>
+                      <cite>Неизвестный Автор</cite>
+                    </blockquote>
+                  <?php elseif ($item['type'] === 'post-text'): ?>
                     <p><?= $item['content'] ?></p>
+                  <?php elseif ($item['type'] === 'post-photo'): ?>
+                    <div class="post-photo__image-wrapper">
+                      <img src="img/<?= $item['content'] ?>" alt="Фото отпользователя" width="360" height="240">
+                    </div>
+                  <?php elseif ($item['type'] === 'post-link'): ?>
+                    <div class="post-link__wrapper">
+                      <a class="post-link__external" href="http://<?= $ite['content'] ?>" title="Перейти по ссылке">
+                          <div class="post-link__info-wrapper">
+                              <div class="post-link__icon-wrapper">
+                                  <img src="" alt="Иконка">
+                              </div>
+                              <div class="post-link__info">
+                                  <h3><?= $item['title'] ?></h3>
+                              </div>
+                          </div>
+                          <span><?= $item['content'] ?></span>
+                      </a>
+                    </div>
+                  <?php endif; ?>
                 </div>
-                <footer class="post__footer">
-                    <div class="post__author">
-                        <a class="post__author-link" href="#" title="Автор">
-                            <div class="post__avatar-wrapper">
-                                <!--укажите путь к файлу аватара-->
-                                <img class="post__author-avatar" src="img/<?= $item['avatar'] ?>" alt="Аватар пользователя">
-                            </div>
-                            <div class="post__info">
-                                <b class="post__author-name"><?= $item['username'] ?></b>
-                                <time class="post__time" datetime="">дата</time>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="post__indicators">
-                        <div class="post__buttons">
-                            <a class="post__indicator post__indicator--likes button" href="#" title="Лайк">
-                                <svg class="post__indicator-icon" width="20" height="17">
-                                    <use xlink:href="#icon-heart"></use>
-                                </svg>
-                                <svg class="post__indicator-icon post__indicator-icon--like-active" width="20" height="17">
-                                    <use xlink:href="#icon-heart-active"></use>
-                                </svg>
-                                <span>0</span>
-                                <span class="visually-hidden">количество лайков</span>
-                            </a>
-                            <a class="post__indicator post__indicator--comments button" href="#" title="Комментарии">
-                                <svg class="post__indicator-icon" width="19" height="17">
-                                    <use xlink:href="#icon-comment"></use>
-                                </svg>
-                                <span>0</span>
-                                <span class="visually-hidden">количество комментариев</span>
-                            </a>
-                        </div>
-                    </div>
-                </footer>
-            </article>
-            <?php endforeach; ?>
+              <footer class="post__footer">
+                  <div class="post__author">
+                      <a class="post__author-link" href="#" title="Автор">
+                          <div class="post__avatar-wrapper">
+                              <!--укажите путь к файлу аватара-->
+                              <img class="post__author-avatar" src="img/<?= $item['avatar'] ?>" alt="Аватар пользователя">
+                          </div>
+                          <div class="post__info">
+                              <b class="post__author-name"><?= $item['username'] ?></b>
+                              <time class="post__time" datetime="">дата</time>
+                          </div>
+                      </a>
+                  </div>
+                  <div class="post__indicators">
+                      <div class="post__buttons">
+                          <a class="post__indicator post__indicator--likes button" href="#" title="Лайк">
+                              <svg class="post__indicator-icon" width="20" height="17">
+                                  <use xlink:href="#icon-heart"></use>
+                              </svg>
+                              <svg class="post__indicator-icon post__indicator-icon--like-active" width="20" height="17">
+                                  <use xlink:href="#icon-heart-active"></use>
+                              </svg>
+                              <span>0</span>
+                              <span class="visually-hidden">количество лайков</span>
+                          </a>
+                          <a class="post__indicator post__indicator--comments button" href="#" title="Комментарии">
+                              <svg class="post__indicator-icon" width="19" height="17">
+                                  <use xlink:href="#icon-comment"></use>
+                              </svg>
+                              <span>0</span>
+                              <span class="visually-hidden">количество комментариев</span>
+                          </a>
+                      </div>
+                  </div>
+              </footer>
+          </article>
+          <?php endforeach; ?>
         </div>
     </div>
 </section>
