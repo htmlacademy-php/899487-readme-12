b0e2779c13b3e65e2ef17aeff30dd26c3a2d54bb
rudserg39 <rudserg39@gmail.com>
2021-05-26 17:15:05 +0200

    Add post time

--- index.php
+++ index.php
@@ -46,28 +46,27 @@
   ];
 
 
-  function trimMessage ($message, $maxDisplayedMessageLength = 300)
-  {
-    $wordsArr = explode(' ', $message);
-    $trimmedWordsArr = [];
-    $messageQuantity = 0;
-    $readMoreButton = '<a class="post-text__more-link" href="#">Читать далее</a>';
+function trimMessage ($message, $maxDisplayedMessageLength = 300)
+{
+  $wordsArr = explode(' ', $message);
+  $trimmedWordsArr = [];
+  $messageQuantity = 0;
+  $readMoreButton = '<a class="post-text__more-link" href="#">Читать далее</a>';
 
-    foreach ($wordsArr as $word) {
-      $messageQuantity += strlen($word);
-      if ($messageQuantity <= $maxDisplayedMessageLength)
-      {
-        array_push($trimmedWordsArr, $word);
-      } else {
-        break;
-      }
+  foreach ($wordsArr as $word) {
+    $messageQuantity += strlen($word);
+    if ($messageQuantity <= $maxDisplayedMessageLength)
+    {
+      array_push($trimmedWordsArr, $word);
+    } else {
+      break;
     }
-
-    $trimmedMessage = implode(' ', $trimmedWordsArr);
-
-    return $messageQuantity > $maxDisplayedMessageLength ? "<p>{$trimmedMessage}...</p>{$readMoreButton}" : "<p>{$trimmedMessage}</p>";
   }
+
+  $trimmedMessage = implode(' ', $trimmedWordsArr);
+
+  return $messageQuantity > $maxDisplayedMessageLength ? "<p>{$trimmedMessage}...</p>{$readMoreButton}" : "<p>{$trimmedMessage}</p>";
+}
 
   echo include_template('layout.php', ['title' => $title, 'user_name' => $user_name, 'is_auth' => $is_auth, 'content' => include_template('main.php', ['array' => $array])]); 
 
-  
